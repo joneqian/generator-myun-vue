ff33619bd201b2021b5f2ecd21c12a0c651b1838
qianqing <qian.qing@aliyun.com>
2016-09-09 20:22:44 +0800

    v0.0.3

--- generators/app/templates/src/module/index/app.vue
+++ generators/app/templates/src/module/index/app.vue
@@ -1,27 +1,16 @@
 <template>
-    <div>
-        <header-component/>
-        <div>this is template body</div>
-        <other-component/>
-    </div>
+  <div>{{msg}}</div>
 </template>
-<style>
-    body{
-        background-color:#ff0000;
-    }
-</style>
+
+<style></style>
+
 <script>
-    import HeaderComponent from './components/header.vue'
-    import OtherComponent from './components/other.vue'
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-        components:{
-            'other-component':OtherComponent,
-            HeaderComponent,
-        }
-    }
+  export default{
+    data(){
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {}
+  }
 </script>
