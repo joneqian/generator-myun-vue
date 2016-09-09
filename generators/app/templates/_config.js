a9f6cf309ea4c3a62d8565cae9bcaa4cba5a279e
qianqing <qian.qing@aliyun.com>
2016-09-09 19:15:06 +0800

    v0.0.3

---
+++ generators/app/templates/_config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by qianqing on 16/9/9.
+ */
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+    index: path.resolve(__dirname, 'dist/index.html'),
+    assetsRoot: path.resolve(__dirname, 'dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: process.env.NODE_ENV === 'production' ? '../' : '/',
+    productionSourceMap: true
+  },
+  dev: {
+    port: 8083,
+    proxyTable: {}
+  }
+}
