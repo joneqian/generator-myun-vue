c13bf610f084d0fcdf1c92fb7b832be3ab46af3f
qianqing <qian.qing@aliyun.com>
2016-09-13 18:12:07 +0800

    v0.0.7

--- generators/app/templates/src/module/index/index.js
+++ generators/app/templates/src/module/index/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by qianqing on 16/9/9.
  */
+import Vue from 'vue';
+import app from './app.vue';
+
+var vm = new Vue({
+  el: 'body',
+  data: {
+  },
+  components: {
+    'app': app
+  },
+  ready: function () {
+  }
+});
